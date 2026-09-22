af1b2ab1e63dd502f97eb1c1ea97ad593c111d36
Thaitheya <sudan25092007@gmail.com>
2026-09-22 12:33:38 +0530

    Smart Table

--- tests/ui-components.spec.ts
+++ tests/ui-components.spec.ts
@@ -1,149 +1,190 @@
-import {test, expect} from '@playwright/test'
+import { test, expect } from '@playwright/test'
 
 
 
-test.beforeEach(async ({page}) => {
-    await page.goto("https://playground.bondaracademy.com/")
+test.beforeEach(async ({ page }) => {
+  await page.goto("https://playground.bondaracademy.com/")
 });
 
-test.describe("Form layouts Page", ()=> {
+test.describe("Form layouts Page", () => {
 
-    test.beforeEach(async({page})=> {
+  test.beforeEach(async ({ page }) => {
     await page.getByRole('link', { name: 'Forms' }).click();
     await page.getByRole('link', { name: 'Form Layouts' }).click();
-    })
+  })
 
-    test('Input Fields', async({page})=> {
-       const usingTheGridEmailInput = page.locator('nb-card', {hasText: "Using the Grid"})
-        .getByRole('textbox', {name: "Email"})
-        await usingTheGridEmailInput.fill("thaitheyasudanpk@gmail.com")
-        await usingTheGridEmailInput.clear()
-        await usingTheGridEmailInput.pressSequentially('thaitheyasudanpk@gmail.com', {delay: 500})
+  test('Input Fields', async ({ page }) => {
+    const usingTheGridEmailInput = page.locator('nb-card', { hasText: "Using the Grid" })
+      .getByRole('textbox', { name: "Email" })
+    await usingTheGridEmailInput.fill("thaitheyasudanpk@gmail.com")
+    await usingTheGridEmailInput.clear()
+    await usingTheGridEmailInput.pressSequentially('thaitheyasudanpk@gmail.com', { delay: 500 })
 
-        await expect(usingTheGridEmailInput).toHaveValue('thaitheyasudanpk@gmail.com')
-        await expect(usingTheGridEmailInput).toHaveValue(/@gmail.com/)
-    })
+    await expect(usingTheGridEmailInput).toHaveValue('thaitheyasudanpk@gmail.com')
+    await expect(usingTheGridEmailInput).toHaveValue(/@gmail.com/)
+  })
 
-    test('Radio Buttons', async({page})=> {
+  test('Radio Buttons', async ({ page }) => {
 
-        const usingTheGridForm = page.locator('nb-card', {hasText: "Using the Grid"})
+    const usingTheGridForm = page.locator('nb-card', { hasText: "Using the Grid" })
 
-        await usingTheGridForm.getByLabel('Option 1').check({force: true});
+    await usingTheGridForm.getByLabel('Option 1').check({ force: true });
 
-        await usingTheGridForm.getByRole('radio', {name: "Option 2"}).check({force: true});
+    await usingTheGridForm.getByRole('radio', { name: "Option 2" }).check({ force: true });
 
-        expect(usingTheGridForm).toBeTruthy()
+    expect(usingTheGridForm).toBeTruthy()
 
-        await expect(usingTheGridForm.getByRole('radio', {name: 'Option 2'})).toBeChecked()
-        await expect(usingTheGridForm.getByRole('radio', {name: 'Option 1'})).not.toBeChecked()
-    })
+    await expect(usingTheGridForm.getByRole('radio', { name: 'Option 2' })).toBeChecked()
+    await expect(usingTheGridForm.getByRole('radio', { name: 'Option 1' })).not.toBeChecked()
+  })
 
 })
 
-test('checkbox', async({page})=> {
- await page.getByText('Modal & Overlays').click()
- await page.getByText('Toastr').click()
+test('checkbox', async ({ page }) => {
+  await page.getByText('Modal & Overlays').click()
+  await page.getByText('Toastr').click()
 
- await page.getByRole('checkbox', {name:'Prevent arising of duplicate toast'}).check({force: true})
+  await page.getByRole('checkbox', { name: 'Prevent arising of duplicate toast' }).check({ force: true })
 
- const allBoxes = page.getByRole('checkbox')
+  const allBoxes = page.getByRole('checkbox')
 
- for(const box of await allBoxes.all()) {
-    await box.uncheck({force: true})
+  for (const box of await allBoxes.all()) {
+    await box.uncheck({ force: true })
     await expect(box).not.toBeChecked()
- }
+  }
 
 })
 
-test("Dropdown and List", async ({page})=> {
+test("Dropdown and List", async ({ page }) => {
 
 
- await page.getByText('Modal & Overlays').click()
- await page.getByText('Toastr').click()
+  await page.getByText('Modal & Overlays').click()
+  await page.getByText('Toastr').click()
 
-//Default Dropdown
- await page.locator('.form-group', {hasText: 'Toast type:'}).getByRole('combobox').selectOption('info')
- await expect(page.getByRole('combobox')).toHaveValue('info')
+  //Default Dropdown
+  await page.locator('.form-group', { hasText: 'Toast type:' }).getByRole('combobox').selectOption('info')
+  await expect(page.getByRole('combobox')).toHaveValue('info')
 
- //Custom Dropdown
+  //Custom Dropdown
 
- await page.locator('.form-group', {hasText: 'Position:'}).locator('nb-select').click()
+  await page.locator('.form-group', { hasText: 'Position:' }).locator('nb-select').click()
 
- //option 1
-//  await page.getByRole('list').getByText('bottom-end').click()
+  //option 1
+  //  await page.getByRole('list').getByText('bottom-end').click()
 
- //Option 2
- await page.locator('nb-option', {hasText: 'bottom-end'}).click()
+  //Option 2
+  await page.locator('nb-option', { hasText: 'bottom-end' }).click()
 
- expect(page.locator('.form-group', {hasText: 'Position:'}).locator('nb-select')).toHaveText('bottom-end')
+  expect(page.locator('.form-group', { hasText: 'Position:' }).locator('nb-select')).toHaveText('bottom-end')
 
- //looping through the list
+  //looping through the list
 
- const positionDropDownField = page.locator('.form-group', {hasText: 'Position:'}).locator('nb-select')
- await positionDropDownField.click()
- const allListValues = await page.locator('nb-option').allTextContents()
- for(const listValues of allListValues) {
-    await page.locator('nb-option', {hasText: listValues}).click()
+  const positionDropDownField = page.locator('.form-group', { hasText: 'Position:' }).locator('nb-select')
+  await positionDropDownField.click()
+  const allListValues = await page.locator('nb-option').allTextContents()
+  for (const listValues of allListValues) {
+    await page.locator('nb-option', { hasText: listValues }).click()
     await expect(positionDropDownField).toHaveText(listValues)
     await positionDropDownField.click()
- }
+  }
 
- console.log(allListValues)
+  console.log(allListValues)
 })
 
-test('tooltip', async ({page})=> {
- await page.getByText('Modal & Overlays').click()
- await page.getByText('Tooltip').click()
+test('tooltip', async ({ page }) => {
+  await page.getByText('Modal & Overlays').click()
+  await page.getByText('Tooltip').click()
 
- await page.getByRole('button', {name: 'Top'}).hover()
- await expect (page.getByRole('tooltip')).toHaveText('This is a tooltip')
+  await page.getByRole('button', { name: 'Top' }).hover()
+  await expect(page.getByRole('tooltip')).toHaveText('This is a tooltip')
 })
 
-test('dialog Box', async({page})=> {
- await page.getByText('Modal & Overlays').click()
- await page.getByText('Dialog').click()
+test('dialog Box', async ({ page }) => {
+  await page.getByText('Modal & Overlays').click()
+  await page.getByText('Dialog').click()
 
-const card = page.locator('nb-card', {
-  hasText: 'Open Dialog with component'
-});
+  const card = page.locator('nb-card', {
+    hasText: 'Open Dialog with component'
+  });
 
-await card.getByRole('button', {
-  name: 'Open Dialog with component',
-  exact: true
-}).click();
+  await card.getByRole('button', {
+    name: 'Open Dialog with component',
+    exact: true
+  }).click();
 
-const dialog = page.locator('nb-card').filter({
-  hasText: 'Dialog'
-});
+  const dialog = page.locator('nb-card').filter({
+    hasText: 'Dialog'
+  });
 
-await dialog
-  .getByRole('button', { name: 'Dismiss Dialog', exact: true })
-  .click();
+  await dialog
+    .getByRole('button', { name: 'Dismiss Dialog', exact: true })
+    .click();
 
 
-const card2 = page.locator('nb-card', {hasText: 'Open with delay 3 seconds'})
+  const card2 = page.locator('nb-card', { hasText: 'Open with delay 3 seconds' })
 
-await card2.getByRole('button', {name: 'Open with delay 3 seconds'}).click()
-await page.waitForTimeout(3000)
+  await card2.getByRole('button', { name: 'Open with delay 3 seconds' }).click()
+  await page.waitForTimeout(3000)
 
-const dialog2 = page.locator('nb-card').filter({
-  hasText: 'OK'
-});
+  const dialog2 = page.locator('nb-card').filter({
+    hasText: 'OK'
+  });
 
-await dialog2
-  .getByRole('button', { name: 'OK', exact: true })
-  .click();
+  await dialog2
+    .getByRole('button', { name: 'OK', exact: true })
+    .click();
 
-}) 
+})
 
-test('Smart Dialog Box', async ({page})=> {
-    await page.getByText('Tables & Data').click()
-    await page.getByText('Smart Table').click()
-    page.on('dialog', dialog => {
-        expect(dialog.message()).toEqual('Are you sure you want to delete?')
-        dialog.accept()
-    })
-    await page.locator('tr', {hasText: 'mdo@gmail.com'}).locator('.nb-trash').click()
-    await expect(page.locator('tr', {hasText:'mdo@gmail.com'})).not.toBeVisible()
+test('Smart Dialog Box', async ({ page }) => {
+  await page.getByText('Tables & Data').click()
+  await page.getByText('Smart Table').click()
+  page.on('dialog', dialog => {
+    expect(dialog.message()).toEqual('Are you sure you want to delete?')
+    dialog.accept()
+  })
+  await page.locator('tr', { hasText: 'mdo@gmail.com' }).locator('.nb-trash').click()
+  await expect(page.locator('tr', { hasText: 'mdo@gmail.com' })).not.toBeVisible()
 
+})
+
+
+test('Table1', async ({ page }) => {
+  await page.getByText('Tables & Data').click()
+  await page.getByText('Smart Table').click()
+
+  //How to select row by any visible text
+  const tableRowByEmail = page.getByRole('row', { name: 'karen@yandex.ru' })
+  await tableRowByEmail.locator('.nb-edit').click()
+  await tableRowByEmail.getByPlaceholder('Age').fill('35')
+  await tableRowByEmail.locator('.nb-checkmark').click()
+
+  await expect(tableRowByEmail.locator('td').last()).toHaveText('35')
+
+  //Get row by a specific column value
+  const tableRowById = page.getByRole('row').filter({ has: page.getByRole('cell').nth(1).getByText('10') })
+  await tableRowById.locator('.nb-edit').click()
+  await page.locator('tbody').getByPlaceholder('E-mail').fill('test@test.com')
+  await page.locator('tbody').locator('.nb-checkmark').click()
+  await expect(tableRowById.locator('td').nth(5)).toHaveText('test@test.com')
+})
+
+test('Table 2', async ({ page }) => {
+  await page.getByText('Tables & Data').click()
+  await page.getByText('Smart Table').click()
+
+  const ages = ["20", "30", "40", "200"]
+  for (let age of ages) {
+    await page.getByPlaceholder('Age').fill(age)
+
+    if (age == "200") {
+      await expect(page.locator('tbody')).toContainText('No data found ')
+    } else {
+      await expect(page.locator('tbody tr').first().locator('td').last()).toHaveText(age)
+      const allTableRows = await page.locator('tbody tr').all()
+      for (let row of allTableRows) {
+        await expect(row.locator('td').last()).toHaveText(age)
+      }
+    }
+  }
 })
